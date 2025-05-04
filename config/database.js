53f0cf03c8f0add7c1307cbf8e2ff930590d8a35
Madhu M <madhu.m@iqlife.ai>
2025-05-04 11:40:22 +0530

    nodejs

--- config/database.js
+++ config/database.js
@@ -1,14 +1,13 @@
-const {createPool}= require('mysql');
+const { createPool } = require('mysql');
 
 
-const pool= createPool({
-    port:process.env.DB_PORT,
-    host:process.env.DB_HORT,
-    user:process.env.DB_USER,
-    password:process.env.DB_PASSWORD,
-    database:process.env.MYSQL_DB,
-    connectionLimit:10
+const pool = createPool({
+    port: process.env.DB_PORT || 3306,     
+    host: process.env.DB_HOST || 'db',      
+    user: process.env.DB_USER || 'root',     
+    password: process.env.DB_PASSWORD || 'example',  
+    database: process.env.MYSQL_DB || 'myapp',     
+    connectionLimit: 10
+});
 
-})
-
-module.exports =pool;
+module.exports = pool;
